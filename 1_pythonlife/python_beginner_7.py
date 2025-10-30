17cfec08b54ceb80dd996f66fe69414dd2a97c05
Varun Tej <varuntej1045@gmail.com>
2025-10-30 23:40:29 +0530

    list variable in python

--- 1_pythonlife/python_beginner_7.py
+++ 1_pythonlife/python_beginner_7.py
@@ -6,5 +6,39 @@ List: []
 - allow indexing
 
 Methods:
-
+append - Add object into the list
+extend - Extends list with multiple objects
+count - count objects inside list
+insert
+pop
+remove - remove element from list
+index
 '''
+
+v=[1, 22, 8.6, 5, 'Kiran']
+print(v)
+print(v[4])
+print(v[-1])
+# Output:
+# [1, 22, 8.6, 5, 'Kiran']
+# Kiran
+# Kiran
+
+print(v[0:4:2]) # [Start, Stop, Skip] - Output: [1, 8.6]
+'''
+start = 0 → start from index 0 (the first element)
+
+stop = 4 → stop before index 4 (so, up to index 3)
+
+step = 2 → take out every 2nd element
+'''
+v.append("Python")
+print(v) # [1, 22, 8.6, 5, 'Kiran', 'Python']
+
+v.extend([5, 25,7.9,'Raj'])
+print(v) # [1, 22, 8.6, 5, 'Kiran', 'Python', 5, 25, 7.9, 'Raj']
+
+print(v.count(5)) # 2
+
+v.remove(22)
+print(v) # [1, 8.6, 5, 'Kiran', 'Python', 5, 25, 7.9, 'Raj']
